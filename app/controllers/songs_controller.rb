275c9fee89db3f478027d19aab7b398b73d4d3aa
Arnold Laoang <arnoldlaoangjr@gmail.com>
2022-04-21 14:54:18 -0700

    Create

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+ def index
+    songs = Song.all
+    render json: songs
+  end
+  def create 
+    create_song = Song.create(song_params)
+    if create_song.valid? 
+        render json: create_song
+    else 
+        render json: create_song.errors
+    end
+  end 
+
+  def new
+  end
+
+  private
+  
+  def song_params
+    params.require(:song).permit(:title, :artist, :user_id)
+  end
+
 end
--- spec/requests/songs_spec.rb
+++ spec/requests/songs_spec.rb
@@ -2,6 +2,39 @@ require 'rails_helper'
 
 RSpec.describe "Songs", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of song" do
+      User.create(
+        email: 'email@email.com'
+      )
+      user= User.find 1
+      expect(user.email).to eq 'email@email.com'
+      # Song.create(
+      #   title: "Alien Boy",
+      #   artist: "Oliver Tree"
+      # )
+      # get '/songs'
+      # song = JSON.parse(response.body)
+      # expect(response).to have_http_status(200)
+      # expect(song.length).to eq 1
+    end
   end
+  #   describe "POST /create" do
+  #     it "add song" do
+  #       song_params = {
+  #         song: {
+  #           title: "Alien Boy",
+  #           artist: "Oliver Tree",
+  #           user_id: 1       
+  #         }
+  #       }
+  #       post '/songs', params: song_params
+        
+  #       expect(response).to have_http_status(200)
+  #       song = Song.first
+  #       p song
+        
+  #       expect(song['title']).to eq "Alien Boy"
+  #   end
+  # end
 end
+  
