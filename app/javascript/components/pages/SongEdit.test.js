7913c5953daf80e62932e6f791f5eda3b84a4ab2
tiffanydoh <tiffanydoh@gmail.com>
2022-04-27 11:00:11 -0700

    some testing

--- app/javascript/components/pages/SongEdit.test.js
+++ app/javascript/components/pages/SongEdit.test.js
@@ -9,9 +9,19 @@ import SongEdit from "./SongEdit";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("When SongEdit renders", () => {
+  let song = {
+    title: "test title",
+    artist: "test artist"
+  }
+  const edit = shallow(<SongEdit song={song} />);
+
   it("displays an h2", () => {
-    const edit = shallow(<SongEdit />);
     const songEditHeading = edit.find("h2").text();
     expect(songEditHeading).toEqual("Update or Delete Song Information");
   });
+
+  xit("displays song title", () => {
+    const titleInput = edit.find("input").get(0)
+    console.log(titleInput)
+  })
 });
