27c6480dee1fe6ba5116e9063bf998bcaba752d6
Arnold Laoang <arnoldlaoangjr@gmail.com>
2022-04-29 09:46:49 -0700

    Worked on the Not Found page

---
+++ app/javascript/components/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+
+export class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="index-name">404 Not Found</h1>
+      </div>
+    );
+  }
+}
+
+export default NotFound
