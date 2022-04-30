3002ce1a1518f744e374e664aa9a800bfe4ee967
RyanJ2463 <jacobellr2017@gmail.comjacobellr2017@gmail.comcd>
2022-04-29 23:07:17 -0400

    more testing

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
 class Song < ApplicationRecord
   belongs_to :user
+  validates :title, :artist, :image, presence: true
+  validates :title, :artist, length: { minimum: 2}
 end
--- spec/requests/songs_spec.rb
+++ spec/requests/songs_spec.rb
@@ -66,8 +66,151 @@ RSpec.describe "Songs", type: :request do
     it "destroys a song" do
       delete "/songs/#{song.id}"
       song_response = JSON.parse(response.body)
+      songs = Song.all
       expect(songs).to be_empty 
     end
   end
 end
-  
+
+describe 'no invalid params' do
+  let(:user) do
+    User.create(
+      email: 'yolo@aol.com', 
+      password: 'coolguy55', 
+      password_confirmation: 'coolguy55'
+    )
+  end
+
+  let!(:song) do
+    Song.create(
+      title: "i miss the old Kanye",
+      artist: "Kanye",
+      image: "test image",
+      user_id: user.id
+    ) 
+  end
+
+  before do 
+    sign_in user
+  end
+
+  it 'does not create a song without a title' do
+    song_params = {
+      song: {
+        artist: 'testing artist',
+        image: 'testing image'
+      }
+    }
+    post '/songs', params: song_params
+    json = JSON.parse(response.body)
+    expect(json['title']).to_not be_empty 
+  end
+
+  it 'does not create a song without a artist' do
+    song_params = {
+      song: {
+        title: 'testing title',
+        image: 'testing image'
+      }
+    }
+    post '/songs', params: song_params
+    json = JSON.parse(response.body)
+    expect(json['artist']).to_not be_empty 
+  end
+
+  it 'does not create a song without a image' do
+    song_params = {
+      song: {
+        title: 'testing title',
+        artist: 'testing artist'
+      }
+    }
+    post '/songs', params: song_params
+    json = JSON.parse(response.body)
+    expect(json['image']).to_not be_empty 
+  end
+
+  it 'does not update a song with empty artist' do
+    song_params = {
+      song: {
+        title: 'testing title',
+        image: 'testing image'
+      }
+    }
+    patch "/songs/#{song.id}", params: song_params
+    json = JSON.parse(response.body)
+    expect(json['artist']).to_not be_empty 
+  end
+
+  it 'does not update a song with empty title' do
+    song_params = {
+      song: {
+        artist: 'testing artist',
+        image: 'testing image'
+      }
+    }
+    patch "/songs/#{song.id}", params: song_params
+    json = JSON.parse(response.body)
+    expect(json['title']).to_not be_empty 
+  end
+
+  it 'does not update a song with empty title' do
+    song_params = {
+      song: {
+        artist: 'testing artist',
+        title: 'testing title'
+      }
+    }
+    patch "/songs/#{song.id}", params: song_params
+    json = JSON.parse(response.body)
+    expect(json['title']).to_not be_empty 
+  end
+
+  it 'does not update a song with a title less than 2 characters' do
+    song_params = {
+      song: {
+        artist: 't',
+        title: 'testing title'
+      }
+    }
+    patch "/songs/#{song.id}", params: song_params
+    json = JSON.parse(response.body)
+    expect(json['artist']).to include 'is too short (minimum is 2 characters)'
+  end
+
+  it 'does not update a song with a title less than 2 characters' do
+    song_params = {
+      song: {
+        artist: 'testing artist',
+        title: 't'
+      }
+    }
+    patch "/songs/#{song.id}", params: song_params
+    json = JSON.parse(response.body)
+    expect(json['title']).to include 'is too short (minimum is 2 characters)'
+  end
+
+  it 'does not create a song without invalid validations' do
+    song_params = {
+      song: {
+        title: 't',
+        artist: 'testing artist'
+      }
+    }
+    post '/songs', params: song_params
+    json = JSON.parse(response.body)
+    expect(json['title']).to include 'is too short (minimum is 2 characters)'
+  end
+
+  it 'does not create a song without invalid validations' do
+    song_params = {
+      song: {
+        title: 'testing title',
+        artist: 't'
+      }
+    }
+    post '/songs', params: song_params
+    json = JSON.parse(response.body)
+    expect(json['artist']).to include 'is too short (minimum is 2 characters)'
+  end
+end
